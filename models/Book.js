f85c3695669767dd3a8996d32bd95b17a1c3541f
nextLine <daniel.attitsogbe02@gmail.com>
2022-08-25 22:31:14 +0000

    add Book model

---
+++ models/Book.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    author: {
+        firstName: String,
+        lastName: String,
+    },
+    genre: String,
+    summary: String,
+    pubDate: Date,
+    createdAt: {
+        type: Date,
+        immutable: true,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
