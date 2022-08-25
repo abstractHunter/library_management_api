1e3c93438dbd28dc654abd8acb1713de8784dbcd
nextLine <daniel.attitsogbe02@gmail.com>
2022-08-25 20:48:52 +0000

    add scalability with cluster module

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
+// imports
 require("dotenv").config();
 const express = require("express");
+const cluster = require("cluster");
+const os = require("os");
+
+// constants
 const app = express();
-const port = process.env.PORT || 3000;
+const nbCPUs = os.cpus().length;
+const config = {
+    port: process.env.PORT,
+}
 
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}`);
-});
+
+if (cluster.isMaster) {
+    for (let i = 0; i < nbCPUs; i++) {
+        cluster.fork();
+    }
+
+    // if a worker dies, create a new one
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        cluster.fork();
+    });
+} else {
+    app.listen(config.port, () => {
+        console.log(`Server ${process.pid} running on port ${config.port}`);
+    });
+}
